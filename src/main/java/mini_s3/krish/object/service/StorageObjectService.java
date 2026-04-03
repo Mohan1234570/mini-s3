16975514779fdbbccbbeb0fcfbd9f03986735589
GMK <mk4400320@gmail.com>
2026-04-03 15:31:19 +0530

    Transactional annotation was added in service layer

--- src/main/java/mini_s3/krish/object/service/StorageObjectService.java
+++ src/main/java/mini_s3/krish/object/service/StorageObjectService.java
@@ -10,6 +10,7 @@ import mini_s3.krish.object.repo.StorageObjectRepository;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class StorageObjectService {
 
